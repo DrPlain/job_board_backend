b86246d9c5be617b19340e52b945875b43bab549
Gideon Obiasor <gideonobiasor@gmail.com>
2025-03-06 18:14:11 +0100

    Update settings.py

--- job_board_backend/settings.py
+++ job_board_backend/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
+# Default primary key
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
