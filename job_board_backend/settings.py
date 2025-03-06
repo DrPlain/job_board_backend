6749f8a2be0f6d9bf53fef8b34ee3398480702ad
Gideon Obiasor <gideonobiasor@gmail.com>
2025-03-06 14:05:05 +0100

    Update settings.py

--- job_board_backend/settings.py
+++ job_board_backend/settings.py
@@ -163,7 +163,7 @@ SIMPLE_JWT = {
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
 
